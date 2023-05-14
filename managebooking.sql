10d7e5531387a0574254c720bdf4f095bdac596f
dcourse99 <120071283+dcourse99@users.noreply.github.com>
2023-05-14 05:36:54 -0400

    Update managebooking.sql

--- managebooking.sql
+++ managebooking.sql
@@ -3,7 +3,7 @@ CREATE PROCEDURE AddValidBooking (IN BookingDate DATE, IN TableNumber INT)
 START TRANSACTION;
 
 SELECT BookingDate, TableNumber
-WHERE exists (SELECT * from Booking where Date = BookingDate and TableNumber = TableNumber)
+WHERE exists (SELECT * from booking where Date = BookingDate and TableNumber = TableNumber)
 
 	INSERT INTO booking (date, table_number)
 	VALUES (BookingDate, TableNumber);
